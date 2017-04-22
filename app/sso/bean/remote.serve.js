af024252913cad331502f453afa3aed0fd3d5261
liguanghao <chiansoft_lgh@outlook.com>
2017-04-22 13:46:46 +0800

    sso

---
+++ app/sso/bean/remote.serve.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by Administrator on 2017/4/22.
+ */
+var RemoteServe = (function () {
+    function RemoteServe() {
+    }
+    return RemoteServe;
+}());
+exports.RemoteServe = RemoteServe;
+//# sourceMappingURL=remote.serve.js.map
